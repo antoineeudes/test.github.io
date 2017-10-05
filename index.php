7d378bdbe4450d3eae33bd1eb2947a07980eeb70
Antoine Eudes <Antoine.eudes@gmail.com>
2017-10-05 11:45:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <img src="champ.jpg">
+    </body>
+</html>
